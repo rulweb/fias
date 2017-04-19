bacd6dd3e67aa55878584cdfccbfd6e57d6c9cdf
Виталий Акмашев <va@crtweb.ru>
2017-04-19 10:14:55 +0500

    add exceptions for curl transport and remove chuncked transport

--- src/transport/Curl.php
+++ src/transport/Curl.php
@@ -13,19 +13,26 @@ class Curl implements ITransport
     public function load($from, \marvin255\fias\file\IFile $file)
     {
         $hLocal = fopen($file->getPath(), 'wb');
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $from);
+        if ($hLocal === false) {
+            throw new Exception("Can't open local file for writing");
+        }
+
+        $ch = curl_init($from);
         curl_setopt($ch, CURLOPT_FILE, $hLocal);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-        $start = microtime(true);
         $res = curl_exec($ch);
-        if ($res === false) {
-            throw new Exception('Error while downloading ' . curl_error($ch));
-        }
-        $time = microtime(true) - $start;
-        printf('Скрипт выполнялся %.4F сек.', $time); echo "\r\n";
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $error = curl_error($ch);
 
+        fclose($hLocal);
         curl_close($ch);
+
+        if ($res === false) {
+            $file->delete();
+            throw new Exception("Error while downloading {$error}");
+        } elseif ($httpCode !== 200) {
+            $file->delete();
+            throw new Exception("Url returns status {$httpCode}");
+        }
     }
 }
--- src/transport/ITransport.php
+++ src/transport/ITransport.php
@@ -7,4 +7,11 @@ namespace marvin255\fias\transport;
  */
 interface ITransport
 {
+    /**
+     * Загружает файл по указанной в первом параметре ссылке на локальный диск в файл, указанный во втором параметре.
+     *
+     * @param string $from
+     * @param \marvin255\fias\file\IFile $file
+     */
+    public function load($from, \marvin255\fias\file\IFile $file);
 }
