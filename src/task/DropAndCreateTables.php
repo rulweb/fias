204bd34c7e671c36c9a2ddc8eb9022f5d9e74ccd
RuleZzz <rul22rus@gmail.com>
2018-05-27 05:33:08 +0300

    - Примари ключи перенёс в запрос создания таблиц - Отключил загрузку и распаковку архива (загрузка не работает, распаковка соответственно не нужна)

--- src/task/DropAndCreateTables.php
+++ src/task/DropAndCreateTables.php
@@ -18,186 +18,203 @@ class DropAndCreateTables implements TaskInterface
         'structure_statuses' => 'CREATE TABLE structure_statuses
         (
             STRSTATID int(11) unsigned not null,
-            NAME varchar(255) not null,
-            SHORTNAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            SHORTNAME varchar(20) not null,
+            PRIMARY KEY(STRSTATID)
+        );
     ',
         'address_object_types' => 'CREATE TABLE address_object_types
         (
-            KOD_T_ST int(11) unsigned not null,
+            KOD_T_ST int(4) unsigned not null,
             LEVEL int(5) unsigned not null,
-            SOCRNAME varchar(255) not null,
-            SCNAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            SOCRNAME varchar(50) not null,
+            SCNAME varchar(10) not null,
+            PRIMARY KEY(KOD_T_ST)
+        );
     ',
         'room_types' => 'CREATE TABLE room_types
         (
             RMTYPEID int(11) unsigned not null,
-            NAME varchar(255) not null,
-            SHORTNAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            SHORTNAME varchar(20) not null,
+            PRIMARY KEY(RMTYPEID)
+        );
     ',
         'operation_statuses' => 'CREATE TABLE operation_statuses
         (
             OPERSTATID int(11) unsigned not null,
-            NAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            PRIMARY KEY(OPERSTATID)
+        );
     ',
         'normative_document_types' => 'CREATE TABLE normative_document_types
         (
             NDTYPEID int(11) unsigned not null,
-            NAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            PRIMARY KEY(NDTYPEID)
+        );
     ',
         'interval_statuses' => 'CREATE TABLE interval_statuses
         (
             INTVSTATID int(11) unsigned not null,
-            NAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            PRIMARY KEY(INTVSTATID)
+        );
     ',
         'house_state_statuses' => 'CREATE TABLE house_state_statuses
         (
             HOUSESTID int(11) unsigned not null,
-            NAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            PRIMARY KEY(HOUSESTID)
+        );
     ',
         'flat_types' => 'CREATE TABLE flat_types
         (
             FLTYPEID int(11) unsigned not null,
-            NAME varchar(255) not null,
-            SHORTNAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            SHORTNAME varchar(20) not null,
+            PRIMARY KEY(FLTYPEID)
+        );
     ',
         'estate_statuses' => 'CREATE TABLE estate_statuses
         (
             ESTSTATID int(11) unsigned not null,
-            NAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            PRIMARY KEY(ESTSTATID)
+        );
     ',
         'current_statuses' => 'CREATE TABLE current_statuses
         (
             CURENTSTID int(11) unsigned not null,
-            NAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            PRIMARY KEY(CURENTSTID)
+        );
     ',
         'center_statuses' => 'CREATE TABLE center_statuses
         (
             CENTERSTID int(11) unsigned not null,
-            NAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            PRIMARY KEY(CENTERSTID)
+        );
     ',
         'actual_statuses' => 'CREATE TABLE actual_statuses
         (
             ACTSTATID int(11) unsigned not null,
-            NAME varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NAME varchar(100) not null,
+            PRIMARY KEY(ACTSTATID)
+        );
     ',
         'steads' => 'CREATE TABLE steads
         (
-            STEADGUID varchar(255) not null,
+            STEADGUID varchar(36) not null,
             `NUMBER` varchar(255) not null,
-            REGIONCODE varchar(255) not null,
-            POSTALCODE varchar(255) not null,
-            IFNSFL varchar(255) not null,
-            IFNSUL varchar(255) not null,
-            OKATO varchar(255) not null,
-            UPDATEDATE varchar(255) not null,
-            PARENTGUID varchar(255) not null,
-            STEADID varchar(255) not null,
+            REGIONCODE varchar(2) not null,
+            POSTALCODE varchar(6) not null,
+            IFNSFL varchar(4) not null,
+            IFNSUL varchar(4) not null,
+            OKATO varchar(11) not null,
+            OKTMO varchar(11) not null,
+            PARENTGUID varchar(36) not null,
+            STEADID varchar(36) not null,
             OPERSTATUS varchar(255) not null,
-            STARTDATE varchar(255) not null,
-            ENDDATE varchar(255) not null,
-            OKTMO varchar(255) not null,
+            STARTDATE date,
+            UPDATEDATE date,
+            ENDDATE date,
             LIVESTATUS varchar(255) not null,
             DIVTYPE varchar(255) not null,
-            NORMDOC varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NORMDOC varchar(36) not null,
+            PRIMARY KEY(STEADGUID)
+        );
     ',
         'rooms' => 'CREATE TABLE rooms
         (
-            ROOMID varchar(255) not null,
-            ROOMGUID varchar(255) not null,
-            HOUSEGUID varchar(255) not null,
-            REGIONCODE varchar(255) not null,
-            FLATNUMBER varchar(255) not null,
-            FLATTYPE varchar(255) not null,
-            POSTALCODE varchar(255) not null,
-            UPDATEDATE varchar(255) not null,
-            STARTDATE varchar(255) not null,
-            ENDDATE varchar(255) not null,
+            ROOMID varchar(36) not null,
+            ROOMGUID varchar(36) not null,
+            HOUSEGUID varchar(36) not null,
+            REGIONCODE varchar(2) not null,
+            FLATNUMBER varchar(50) not null,
+            FLATTYPE int(11) not null,
+            POSTALCODE varchar(6) not null,
+            UPDATEDATE date,
+            STARTDATE date,
+            ENDDATE date,
             OPERSTATUS varchar(255) not null,
             LIVESTATUS varchar(255) not null,
-            NORMDOC varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            NORMDOC varchar(36) not null,
+            PRIMARY KEY(ROOMID)
+        );
     ',
         'normative_documents' => 'CREATE TABLE normative_documents
         (
-            NORMDOCID varchar(255) not null,
+            NORMDOCID varchar(36) not null,
             DOCNAME text not null,
             DOCDATE varchar(255) not null,
             DOCNUM varchar(255) not null,
-            DOCTYPE varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            DOCTYPE varchar(255) not null,
+            PRIMARY KEY(NORMDOCID)
+        );
     ',
         'houses' => 'CREATE TABLE houses
         (
-            HOUSEID varchar(255) not null,
-            HOUSEGUID varchar(255) not null,
-            AOGUID varchar(255) not null,
-            HOUSENUM varchar(255) not null,
-            STRSTATUS varchar(255) not null,
-            ESTSTATUS varchar(255) not null,
-            STATSTATUS varchar(255) not null,
-            IFNSFL varchar(255) not null,
-            IFNSUL varchar(255) not null,
-            OKATO int(15) not null,
-            OKTMO varchar(255) not null,
-            POSTALCODE varchar(255) not null,
-            STARTDATE varchar(255) not null,
-            ENDDATE varchar(255) not null,
-            UPDATEDATE varchar(255) not null,
-            COUNTER varchar(255) not null,
-            DIVTYPE varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci PARTITION BY LINEAR HASH (OKATO) PARTITIONS 4;
-    ',
-        'address_objects' => 'CREATE TABLE address_objects
-        (
-            AOID varchar(255) not null,
-            AOGUID varchar(255) not null,
-            PARENTGUID varchar(255) not null,
-            NEXTID varchar(255) not null,
-            FORMALNAME varchar(255) not null,
-            OFFNAME varchar(255) not null,
-            SHORTNAME varchar(255) not null,
-            AOLEVEL int(11) unsigned not null,
-            REGIONCODE varchar(255) not null,
-            AREACODE varchar(255) not null,
-            AUTOCODE varchar(255) not null,
-            CITYCODE varchar(255) not null,
-            CTARCODE varchar(255) not null,
-            PLACECODE varchar(255) not null,
-            PLANCODE varchar(255) not null,
-            STREETCODE varchar(255) not null,
-            EXTRCODE varchar(255) not null,
-            SEXTCODE varchar(255) not null,
-            PLAINCODE varchar(255) not null,
-            CURRSTATUS varchar(255) not null,
-            ACTSTATUS varchar(255) not null,
-            LIVESTATUS varchar(255) not null,
-            CENTSTATUS varchar(255) not null,
-            OPERSTATUS varchar(255) not null,
-            IFNSFL varchar(255) not null,
-            IFNSUL varchar(255) not null,
-            TERRIFNSFL varchar(255) not null,
-            TERRIFNSUL varchar(255) not null,
-            OKATO varchar(255) not null,
-            OKTMO varchar(255) not null,
-            POSTALCODE varchar(255) not null,
+            HOUSEID varchar(36) not null,
+            HOUSEGUID varchar(36) not null,
+            AOGUID varchar(36) not null,
+            HOUSENUM varchar(20) not null,
+            STRSTATUS int(11) not null,
+            ESTSTATUS int(11) not null,
+            STATSTATUS int(11) not null,
+            IFNSFL varchar(4) not null,
+            IFNSUL varchar(4) not null,
+            OKATO varchar(11) not null,
+            OKTMO varchar(11) not null,
+            POSTALCODE varchar(6) not null,
             STARTDATE date,
             ENDDATE date,
             UPDATEDATE date,
-            DIVTYPE varchar(255) not null
-        ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+            COUNTER int(11) not null,
+            DIVTYPE int(11) not null,
+            PRIMARY KEY(HOUSEID)
+        );
+    ',
+        'address_objects' => 'CREATE TABLE address_objects
+        (
+            AOID varchar(36) not null,
+            AOGUID varchar(36) not null,
+            PARENTGUID varchar(36) not null,
+            NEXTID varchar(36) not null,
+            FORMALNAME varchar(120) not null,
+            OFFNAME varchar(120) not null,
+            SHORTNAME varchar(10) not null,
+            AOLEVEL int(11) unsigned not null,
+            REGIONCODE varchar(2) not null,
+            AREACODE varchar(3) not null,
+            AUTOCODE varchar(1) not null,
+            CITYCODE varchar(3) not null,
+            CTARCODE varchar(3) not null,
+            PLACECODE varchar(3) not null,
+            PLANCODE varchar(4) not null,
+            STREETCODE varchar(4) not null,
+            EXTRCODE varchar(4) not null,
+            SEXTCODE varchar(3) not null,
+            PLAINCODE varchar(15) not null,
+            CURRSTATUS int(11) not null,
+            ACTSTATUS int(11) not null,
+            LIVESTATUS int(11) not null,
+            CENTSTATUS int(11) not null,
+            OPERSTATUS int(11) not null,
+            IFNSFL varchar(4) not null,
+            IFNSUL varchar(4) not null,
+            TERRIFNSFL varchar(4) not null,
+            TERRIFNSUL varchar(4) not null,
+            OKATO varchar(11) not null,
+            OKTMO varchar(11) not null,
+            POSTALCODE varchar(6) not null,
+            STARTDATE date,
+            ENDDATE date,
+            UPDATEDATE date,
+            DIVTYPE int(11) not null,
+            PRIMARY KEY(AOID)
+        );
     ',
     ];
 
